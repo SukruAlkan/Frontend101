8b9179ca73d2d4efa9d7a7b75389a96a5752f73c
sukru.alkan <sukru.alkan@tarnet.com.tr>
2022-09-21 17:03:24 +0300

    hello world

---
+++ 04-javascript-kategorisi/js/001-hello-world.js
@@ -0,0 +1,12 @@
+// ********** Merhaba Dunya ********** 
+
+// Dokumana(document) Yazdirma:
+// document.write('Merhaba Dunya');
+
+// Uyari Mesaji (Alert):
+// alert("Merhaba Dunya")
+
+// console.log ile
+console.log("Merhaba Dunya")
+
+//node .\001-hello-world.js
